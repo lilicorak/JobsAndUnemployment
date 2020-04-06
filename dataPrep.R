29d6dad6c0cc4e134989049d0562c89e1cce0a95
lilicorak <62550305+lilicorak@users.noreply.github.com>
2020-04-06 16:54:46 -0400

    Fetch data directly from StatsCan using CANSIM package

--- dataPrep.R
+++ dataPrep.R
@@ -1,51 +1,55 @@
 
-#install.packages("plyr")
+#install.packages("plyr", "cansim")
 
 require(plyr)
+require(cansim)
 
 # unemployment data
 # seasonally adjusted unemployment data
-adjUnemp <- subset(read.csv("workingData/14100287.csv", head=TRUE, sep=","), 
-                    select=c(REF_DATE, GEO, Labour.force.characteristics, Sex, Age.group, Statistics, Data.type, VALUE),
-                      Labour.force.characteristics %in% c("Unemployment", "Unemployment rate") &
+adjUnemp <- subset(#read.csv("workingData/14100287.csv", head=TRUE, sep=","), 
+                    get_cansim("14-10-0287"),
+                    select=c(REF_DATE, GEO, `Labour force characteristics`, Sex, `Age group`, Statistics, `Data type`, VALUE),
+                    `Labour force characteristics` %in% c("Unemployment", "Unemployment rate") &
                       Statistics == "Estimate" &
-                      Data.type == "Seasonally adjusted" &
-                      Age.group %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
+                      `Data type` == "Seasonally adjusted" &
+                      `Age group` %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
 
-adjUnemp$Statistics <- revalue(adjUnemp$Labour.force.characteristics,
+adjUnemp$Statistics <- revalue(adjUnemp$`Labour force characteristics`,
                                c("Unemployment" = "Number unemployed (x1,000)", 
                                  "Unemployment rate" = "Official unemployment rate, seasonally adjusted"))
 
 adjUnemp$refPeriod <- as.Date(paste0(adjUnemp$REF_DATE,"-01"))
 
-adjUnemp <- subset(adjUnemp, select=-c(REF_DATE, Labour.force.characteristics, Data.type))
+adjUnemp <- subset(adjUnemp, select=-c(REF_DATE, `Labour force characteristics`, `Data type`))
 
 
 # non-seasonally adjusted supplementary unemployment rates
-suppUnemp <- subset(read.csv("workingData/14100077.csv", head=TRUE, sep=","), 
-                        select=c(REF_DATE, GEO, Supplementary.unemployment.rates, Sex, Age.group, VALUE),
-                        Supplementary.unemployment.rates %in% c("R4 - official rate", 
+suppUnemp <- subset(#read.csv("workingData/14100077.csv", head=TRUE, sep=","), 
+                    get_cansim("14-10-0077"),
+                        select=c(REF_DATE, GEO, `Supplementary unemployment rates`, Sex, `Age group`, VALUE),
+                        `Supplementary unemployment rates` %in% c("R4 - official rate", 
                                                                 "R8 - plus discouraged searchers, waiting group, portion of involuntary part-timers") &
-                          Age.group %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
+                          `Age group` %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
 
-suppUnemp$Statistics <- revalue(suppUnemp$Supplementary.unemployment.rates, 
+suppUnemp$Statistics <- revalue(suppUnemp$`Supplementary unemployment rates`, 
                                             c("R4 - official rate" = "Official unemployment rate, not seasonally adjusted",
                                               "R8 - plus discouraged searchers, waiting group, portion of involuntary part-timers" =
                                                 "Comprehensive unemployment rate, not seasonally adjusted "))
 
 suppUnemp$refPeriod <- as.Date(paste0(suppUnemp$REF_DATE,"-01"))
 
-suppUnemp <- subset(suppUnemp, select=-c(REF_DATE, Supplementary.unemployment.rates))
+suppUnemp <- subset(suppUnemp, select=-c(REF_DATE, `Supplementary unemployment rates`))
 
 
 # unemployment by reason for leaving job
-reasonUnemp <- subset(read.csv("workingData/14100125.csv", head=TRUE, sep=","), 
-                    select=c(REF_DATE, GEO, Reason, Characteristics, Sex, Age.group, VALUE),
-                    Reason != "Have not worked in the last year" &
+reasonUnemp <- subset(#read.csv("workingData/14100125.csv", head=TRUE, sep=","), 
+                      get_cansim("14-10-0125"),
+                      select=c(REF_DATE, GEO, Reason, Characteristics, Sex, `Age group`, VALUE),
+                      Reason != "Have not worked in the last year" &
                       Reason != "Never worked" &
                       Reason != "Total, all reasons" &
                       Characteristics == "Unemployed" &
-                      Age.group %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
+                      `Age group` %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
 
 reasonUnemp$Statistics <- reasonUnemp$Reason
 
@@ -54,18 +58,19 @@ reasonUnemp$refPeriod <- as.Date(paste0(reasonUnemp$REF_DATE,"-01"))
 reasonUnemp <- subset(reasonUnemp, select=-c(REF_DATE, Reason, Characteristics))
 
 # short unemployment estimate
-shortUnemp <- subset(read.csv("workingData/14100342.csv", head=TRUE, sep=","), 
-                   select=c(REF_DATE, GEO, Duration.of.unemployment, Sex, Age.group, Statistics, Data.type, VALUE),
-                     Duration.of.unemployment == "1 to 4 weeks" &
+shortUnemp <- subset(#read.csv("workingData/14100342.csv", head=TRUE, sep=","), 
+                     get_cansim("14-10-0342"),
+                     select=c(REF_DATE, GEO, `Duration of unemployment`, Sex, `Age group`, Statistics, `Data type`, VALUE),
+                     `Duration of unemployment` == "1 to 4 weeks" &
                      Statistics == "Estimate" &
-                     Data.type == "Seasonally adjusted" &
-                     Age.group %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
+                     `Data type` == "Seasonally adjusted" &
+                     `Age group` %in% c("15 years and over", "15 to 24 years", "25 to 54 years", "55 years and over"))
 
 shortUnemp$Statistics <- "Number unemployed one month or less (x1,000)"
 
 shortUnemp$refPeriod <- as.Date(paste0(shortUnemp$REF_DATE,"-01"))
 
-shortUnemp <- subset(shortUnemp, select=-c(REF_DATE, Duration.of.unemployment, Data.type))
+shortUnemp <- subset(shortUnemp, select=-c(REF_DATE, `Duration of unemployment`, `Data type`))
 
 # export final unemployment file
 unempData <- rbind(adjUnemp, suppUnemp, reasonUnemp, shortUnemp)
@@ -83,7 +88,7 @@ write.csv(unempData, file="data/unempFinalData.csv",na="",row.names = F)
 
 # employment data by NAICS
 # naicsEmp <- subset(read.csv("workingData/14100355.csv", head=TRUE, sep=","), 
-#                                 select=c(REF_DATE, GEO, Supplementary.unemployment.rates, Sex, Age.group, VALUE))
+#                                 select=c(REF_DATE, GEO, `Supplementary unemployment rates`, Sex, `Age group`, VALUE))
 
 
 
